35e086818785d15d01a0591a9c311289b2b69a5c
dhofstetter <dh@yio.at>
2026-08-05 07:12:32 +0200

    fix(docker): fix some wrong legacy paths for startup (#22)

--- docker-images/general/start.py
+++ docker-images/general/start.py
@@ -2,15 +2,15 @@ import os
 
 import squirrel
 
-minion_module_name = os.getenv("MODULE_NAME", "minion")
-minion_variable_name = os.getenv("VARIABLE_NAME", "minion")
+module_name = os.getenv("MODULE_NAME", "app")
+variable_name = os.getenv("VARIABLE_NAME", "app")
 
 # check for legacy compatibility
-if ":" not in minion_module_name:
+if ":" not in module_name:
     print("old MODULE_NAME only invocation detected, going to add variable name")
-    app = f"{minion_module_name}:{minion_variable_name}"
+    app = f"{module_name}:{variable_name}"
 else:
-    app = minion_module_name
+    app = module_name
 
 if __name__ == "__main__":
     squirrel.run(app)
--- docker-images/general/start.sh
+++ docker-images/general/start.sh
@@ -3,13 +3,13 @@ set -e
 
 # Try to find a minion application that can be started
 # And add appropriate values to some environment variables
-if [ -f app/app/fastbpmn.py ]; then
-    DEFAULT_MODULE_NAME=app.minion
-elif [ -f app/fastbpmn.py ]; then
-    DEFAULT_MODULE_NAME=minion
+if [ -f app/app/app.py ]; then
+    DEFAULT_MODULE_NAME=app.app
+elif [ -f app/app.py ]; then
+    DEFAULT_MODULE_NAME=app
 fi
 export MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
-export VARIABLE_NAME=${VARIABLE_NAME:-minion}
+export VARIABLE_NAME=${VARIABLE_NAME:-app}
 export PYTHONPATH=$PYTHONPATH:app
 
 # If there's a prestart.sh script in the
@@ -25,9 +25,9 @@ fi
 
 # if there's a virtual environment we going to activate it
 # which makes installations of own packages possible and easier
-if [ -d /home/minion/venv ] ; then
+if [ -d /home/fastbpmn/venv ] ; then
     echo "Activating virtual environment"
-    . /home/minion/venv/bin/activate
+    . /home/fastbpmn/venv/bin/activate
 fi
 
 echo "Working directory: $(pwd)"
@@ -43,11 +43,11 @@ fi
 
 # Try to infer whether a real script is given for startup or not
 if [ -f "$1" ] && [ -x "$1" ] ; then
-    # The command seems to be overriden, so we simply start using the given args.
-    echo "Detected overriden startup options going to use them."
+    # The command seems to be overridden, so we simply start using the given args.
+    echo "Detected overridden startup options going to use them."
     exec "$@"
 else
     # Seems like we have to startup a regular minion, so we try to use our start.py script
-    echo "Use the integrated start.py script to startup a minion."
-    exec python3 /home/minion/start.py "$@"
+    echo "Use the integrated start.py script to startup fastbpmn."
+    exec python3 /home/fastbpmn/start.py "$@"
 fi
